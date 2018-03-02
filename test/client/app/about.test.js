e71a7c12d2a2641efd53d54d06a831d741c16745
Aaron Pennington <smokeyblues@hotmail.com>
2018-03-02 10:29:14 -0700

    commented out about component test until I am done writing it and have coverage issue solved

--- test/client/app/about.test.js
+++ test/client/app/about.test.js
@@ -1,9 +1,9 @@
-const chai = require('chai');
-const should = chai.should();
-const expect = chai.expect;
-
-describe('about component', function() {
-    context('about component', function() {
-        it('should')
-    });
-});
+// const chai = require('chai');
+// const should = chai.should();
+// const expect = chai.expect;
+//
+// describe('about component', function() {
+//     context('about component', function() {
+//         it('should')
+//     });
+// });
